f58b0a08df707fd1e9c2c2d887ced075a50f6a87
gabTeodoro <teodoro.gt@hotmail.com>
2020-12-24 18:57:45 -0300

    Updated AulaThread; added method Thread() and run()

--- src/thread/AulaThread.java
+++ src/thread/AulaThread.java
@@ -1,21 +1,43 @@
 package thread;
 
+import javax.swing.JOptionPane;
+
+import executavel.MediaFinal;
+
 public class AulaThread {
 
 	public static void main(String[] args) {
-		
-		for(int pos = 0; pos < 10; pos++) {
-			
-			System.out.println("Executando..");
-			
-			try {
-				Thread.sleep(1500);
-			} catch (InterruptedException e) {
-				System.out.println("Causa do erro: " + e.getMessage());
-				e.printStackTrace();
+
+		JOptionPane.showMessageDialog(null, "Tarefa sendo iniciada.");
+
+		// Instânciando uma Tread para executar um código em paralelo com outro.
+		new Thread() {
+
+			// Executar o código que queremos em segundo plano.
+			public void run() {
+
+				for (int pos = 0; pos < 10; pos++) {
+
+					System.out.println("Executando..");
+
+					// Usando o sleep para executar o método a cada n milisegundos.
+					// Obrigatório usar um try catch.
+					try {
+						Thread.sleep(4000);
+					} catch (InterruptedException e) {
+						System.out.println("Causa do erro: " + e.getMessage());
+						e.printStackTrace();
+					}
+
+				}
+
 			}
-			
-		}
+
+		// Iniciando a Tread
+		}.start();
+
+		// Avisando que o precesso já está sendo executado para o usuário.
+		JOptionPane.showMessageDialog(null, "Processo já está sendo executado! \nObrigado por usar Java.");
 		
 	}
 
